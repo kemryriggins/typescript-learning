47ec25272c3bda9ae8f3131138e710d70272e140
kemryriggins <98kariggins@gmail.com>
2018-01-30 14:09:01 -0700

    finished the arguments functions

--- 016_function_args.js
+++ 016_function_args.js
@@ -1,11 +1,23 @@
-function printAddress(street, streetTwo, state) {
-    if (state === void 0) { state = 'CO'; }
-    console.log(street);
-    if (streetTwo) {
-        console.log(state);
+// function printAddress(street: string, streetTwo?: string, state= 'CO') {
+//	console.log(street);
+//	if (streetTwo) {
+//		console.log(streetTwo);
+//	}
+//	console.log(state);
+// }
+// printAddress('710 Street');
+// printAddress('710 Gone Bye Bye Street', 'Suite 420');
+// printAddress('710 Gone Bye Bye Street', 'Suite 420', 'UT');
+function lineupCard(team) {
+    var players = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        players[_i - 1] = arguments[_i];
+    }
+    console.log('Team: ' + team);
+    for (var _a = 0, players_1 = players; _a < players_1.length; _a++) {
+        var player = players_1[_a];
+        console.log(player);
     }
 }
-printAddress('710 Street');
-printAddress('710 Gone Bye Bye Street', 'Suite 420');
-printAddress('710 Gone Bye Bye Street', 'Suite 420', 'UT');
+lineupCard('Astros', 'Altuve', 'Correra', 'Bregman');
 //# sourceMappingURL=016_function_args.js.map
